2bde93eaf1c54532793bb7c97badb37bb2a52690
Will Smith <lwilsonsmith@gmail.com>
2019-04-03 17:00:44 -0400

    Add create-react-app for website with landing page component

---
+++ website/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+//core
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+//front10 components
+import "@front10/landing-page-book/dist/themes/default/index.legacy_browsers.css";
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  Row,
+  Header,
+  Container,
+  Hero,
+  Image,
+  Link,
+  GithubButton,
+  Column,
+  Navbar,
+  NavbarCollapse,
+  NavbarBrand,
+  NavbarNav,
+  NavbarLink,
+  Icon,
+  LanguageSwitcher,
+  Features,
+  Team,
+  BrowserSupport,
+  Gif,
+  Footer,
+  Copyright,
+  Social
+} from "@front10/landing-page-book/dist/components";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
