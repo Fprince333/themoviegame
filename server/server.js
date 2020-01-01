4c46f7db6ebd3d6f9b905c9f41c8aed36cbd210e
Will Smith <lwilsonsmith@gmail.com>
2020-01-01 15:56:11 -0500

    Game On

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require("dotenv").config();
 
 var users = [];
+var gameOn = false;
 
 var pusher = new Pusher({
   // connect to pusher
@@ -26,7 +27,8 @@ app.get("/", function (req, res) {
 app.post("/pusher/auth", function (req, res) {
   var username = req.body.username;
   console.log(username)
-  if (users.length < 2) {
+  if (users.length < 2 && !gameOn) {
+    gameOn = false;
     var player = {
       name: username,
       channel: req.body.channel_name
@@ -42,7 +44,7 @@ app.post("/pusher/auth", function (req, res) {
   if (users.length === 2) {
     var player_one = users.splice(0, 1)[0];
     var player_two = users.splice(0, 1)[0];
-
+    gameOn = true;
     // trigger a message to player one and player two on their own channels
     console.log("triggering game for: ")
     console.log(player_one.name)
